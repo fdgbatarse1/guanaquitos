bfda886c9b9120d8bb4ce17130404737cd183150
batarse1 <00014419@uca.edu.sv>
2024-02-12 21:31:39 -0600

    ✨ add university content type

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,19 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface MapMap extends Schema.Component {
+  collectionName: 'components_map_maps';
+  info: {
+    displayName: 'Map';
+    icon: 'pinMap';
+    description: '';
+  };
+  attributes: {
+    map: Attribute.JSON &
+      Attribute.CustomField<'plugin::google-maps.location-picker'>;
+    address: Attribute.Text;
+  };
+}
+
 export interface TextText extends Schema.Component {
   collectionName: 'components_text_texts';
   info: {
@@ -15,6 +29,7 @@ export interface TextText extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'map.map': MapMap;
       'text.text': TextText;
     }
   }
