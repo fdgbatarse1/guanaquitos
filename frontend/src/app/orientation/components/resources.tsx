ca221096ecee56330444da2bb74e8f3eb3bd478f
batarse1 <00014419@uca.edu.sv>
2024-05-15 11:21:39 -0600

    :sparkles: Add Resource component for displaying resources with title, description, and link

--- frontend/src/app/orientation/components/resources.tsx
+++ frontend/src/app/orientation/components/resources.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useQuery } from '@apollo/client';
-import { Grid } from '@mui/material';
+import { Grid, Typography, useTheme } from '@mui/material';
 
 import Pagination from '@/components/pagination';
 import Loading from '@/components/loading';
@@ -10,7 +10,9 @@ import resourcesQuery from '@/services/gql/resourcesQuery';
 import resourcesSearchQuery from '@/services/gql/resourcesSearchQuery';
 import { GetResourcesQuery } from '@/gql/graphql';
 import ComplexSearch from '@/components/complex-search';
+import { spacing3 } from '@/styles/spacing';
 
+import Resource from './resource';
 import getFilters from '../utils/getFilters';
 import getFetchOptions from '../utils/getFetchOptions';
 
@@ -30,6 +32,7 @@ const Resources = ({ searchParams }: OrientationProps) => {
   const type = searchParams?.type || '';
   const order = searchParams?.order || '';
   const currentPage = Number(searchParams?.page) || 1;
+  const theme = useTheme();
 
   const { loading, error, data } = useQuery<GetResourcesQuery>(resourcesQuery, {
     variables: {
@@ -57,22 +60,35 @@ const Resources = ({ searchParams }: OrientationProps) => {
   });
 
   return (
-    <Grid container gap={4} direction="column" alignItems="center">
-      <ComplexSearch
-        query={query}
-        fetchOptions={fetchOptions}
-        filters={filters}
-        showFilters={showFilters}
-      />
-      <Grid container spacing={2}>
-        {data?.resources?.data.map((resource: any) => (
-          <Grid key={resource.id} item xs={12} md={4}>
-            {resource?.attributes?.title}
-          </Grid>
-        ))}
+    <div style={{ width: '100%' }}>
+      <Typography variant="h3" color={theme.palette.primary.main}>
+        Asesores
+      </Typography>
+      <Grid container gap={4} direction="column" alignItems="center" sx={{ marginTop: spacing3 }}>
+        <ComplexSearch
+          query={query}
+          fetchOptions={fetchOptions}
+          filters={filters}
+          showFilters={showFilters}
+        />
+        <Grid container spacing={2}>
+          {data?.resources?.data.map((resource) => {
+            const title = resource?.attributes?.title || '';
+            const description = resource?.attributes?.description || '';
+            const url = resource?.attributes?.media?.data?.attributes?.url
+              ? resource.attributes.media.data.attributes.url
+              : resource?.attributes?.link || '';
+            const itemType = resource?.attributes?.type || '';
+            return (
+              <Grid key={resource.id} item xs={12} md={4}>
+                <Resource title={title} description={description} url={url} type={itemType} />
+              </Grid>
+            );
+          })}
+        </Grid>
+        <Pagination count={totalPages} page={currentPage} />
       </Grid>
-      <Pagination count={totalPages} page={currentPage} />
-    </Grid>
+    </div>
   );
 };
 
