d74d1f0def05f2b7e56c4a0faa5bdfe2590b496b
batarse1 <00014419@uca.edu.sv>
2024-06-05 18:10:22 -0600

    :sparkles: Add pg dependency version constraint to package.json and langsmith to career lifecycle

--- backend/src/api/career/content-types/career/lifecycles.ts
+++ backend/src/api/career/content-types/career/lifecycles.ts
@@ -2,49 +2,52 @@ import { Pinecone } from "@pinecone-database/pinecone";
 
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { Document } from "@langchain/core/documents";
+import { traceable } from "langsmith/traceable";
+import { awaitAllCallbacks } from "@langchain/core/callbacks/promises";
 
 const OPENAI_SECRET_KEY = process.env.OPENAI_SECRET_KEY;
 const PINECONE_INDEX = process.env.PINECONE_INDEX;
 const PINECONE_API_KEY = process.env.PINECONE_API_KEY;
 
+const pinecone = new Pinecone({
+  apiKey: PINECONE_API_KEY,
+});
+
+const pineconeIndex = pinecone.Index(PINECONE_INDEX);
+
+const textEmbedding3Small = new OpenAIEmbeddings({
+  apiKey: OPENAI_SECRET_KEY,
+  model: "text-embedding-3-small",
+});
+
 export default {
-  async afterUpdate(event) {
-    const { id } = event.result;
+  afterUpdate: traceable(
+    async (event) => {
+      const { id } = event.result;
 
-    const career = await strapi.entityService.findOne(
-      "api::career.career",
-      id,
-      {
-        populate: ["university"],
-      }
-    );
+      const career = await strapi.entityService.findOne(
+        "api::career.career",
+        id,
+        {
+          populate: ["university"],
+        }
+      );
 
-    try {
-      const { result } = event;
+      try {
+        const { result } = event;
 
-      const pinecone = new Pinecone({
-        apiKey: PINECONE_API_KEY,
-      });
-      const pineconeIndex = pinecone.Index(PINECONE_INDEX);
+        if (result.publishedAt) {
+          const careerMetadata = {
+            id: result?.id,
+            name: result?.name,
+            links: result?.links ? JSON.stringify(result?.links) : undefined,
+            createdAt: result?.createdAt,
+            updatedAt: result?.updatedAt,
+            publishedAt: result?.publishedAt,
+            locale: result?.locale,
+          };
 
-      if (result.publishedAt) {
-        const textEmbedding3Small = new OpenAIEmbeddings({
-          apiKey: OPENAI_SECRET_KEY,
-          model: "text-embedding-3-small",
-        });
-        strapi.log.debug("OPENAI_SECRET_KEY");
-
-        const careerMetadata = {
-          id: result?.id,
-          name: result?.name,
-          links: result?.links ? JSON.stringify(result?.links) : undefined,
-          createdAt: result?.createdAt,
-          updatedAt: result?.updatedAt,
-          publishedAt: result?.publishedAt,
-          locale: result?.locale,
-        };
-
-        const careerPageContent = `
+          const careerPageContent = `
           Nombre de la carrera: ${result?.name || "Desconocido"}.
           Titulo que se obtiene: ${result?.title || "Desconocido"}.
           Grado académico: ${result?.academic_grade || "Desconocido"}.
@@ -84,40 +87,52 @@ export default {
           }.
         `;
 
-        const doc = new Document({
-          pageContent: careerPageContent,
-          metadata: careerMetadata,
-        });
-        strapi.log.debug(JSON.stringify(doc));
-
-        const embeddings = await textEmbedding3Small.embedDocuments([
-          JSON.stringify(doc),
-        ]);
-
-        await pineconeIndex.upsert([
-          {
-            id: `${result?.id}`,
-            values: embeddings[0],
+          const doc = new Document({
+            pageContent: careerPageContent,
             metadata: careerMetadata,
-          },
-        ]);
-      } else {
-        await pineconeIndex.deleteOne(`${result?.id}`);
+          });
+
+          const embeddings = await textEmbedding3Small.embedDocuments([
+            JSON.stringify(doc),
+          ]);
+
+          await pineconeIndex.upsert([
+            {
+              id: `${result?.id}`,
+              values: embeddings[0],
+              metadata: careerMetadata,
+            },
+          ]);
+        } else {
+          await pineconeIndex.deleteOne(`${result?.id}`);
+        }
+      } catch (error) {
+        strapi.log.error(error);
+      } finally {
+        await awaitAllCallbacks();
       }
-    } catch (error) {
-      strapi.log.error(error);
+    },
+    {
+      name: "guanaquitos_upsert_career",
     }
-  },
-  async afterDelete(event) {
-    try {
-      const { result } = event;
-      const pinecone = new Pinecone({
-        apiKey: PINECONE_API_KEY,
-      });
-      const pineconeIndex = pinecone.Index(PINECONE_INDEX);
-      await pineconeIndex.deleteOne(`${result?.id}`);
-    } catch (error) {
-      strapi.log.error(error);
+  ),
+  afterDelete: traceable(
+    async (event) => {
+      try {
+        const { result } = event;
+        const pinecone = new Pinecone({
+          apiKey: PINECONE_API_KEY,
+        });
+        const pineconeIndex = pinecone.Index(PINECONE_INDEX);
+        await pineconeIndex.deleteOne(`${result?.id}`);
+      } catch (error) {
+        strapi.log.error(error);
+      } finally {
+        await awaitAllCallbacks();
+      }
+    },
+    {
+      name: "guanaquitos_delete_career",
     }
-  },
+  ),
 };
