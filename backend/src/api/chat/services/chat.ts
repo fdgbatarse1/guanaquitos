b6d40fea1d560e8ff4d3b3320ee5527c55569685
batarse1 <00014419@uca.edu.sv>
2024-06-05 18:17:14 -0600

    🔥 Remove comments from chat

--- backend/src/api/chat/services/chat.ts
+++ backend/src/api/chat/services/chat.ts
@@ -1,11 +1,7 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { ChatOpenAI } from "@langchain/openai";
-import { PineconeStore, PineconeTranslator } from "@langchain/pinecone";
-import type { AttributeInfo } from "langchain/chains/query_constructor";
-import { SelfQueryRetriever } from "langchain/retrievers/self_query";
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
-import { StringOutputParser } from "@langchain/core/output_parsers";
+import { PineconeStore } from "@langchain/pinecone";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { InMemoryChatMessageHistory } from "@langchain/core/chat_history";
 import { RunnableWithMessageHistory } from "@langchain/core/runnables";
@@ -43,21 +39,11 @@ const searchTool = new TavilySearchResults({
   apiKey: TAVILY_API_KEY,
 });
 
-// const attributeInfo: AttributeInfo[] = [
-//   {
-//     name: "name",
-//     description: "The name of the career",
-//     type: "string",
-//   },
-// ];
-
 const Service = () => ({
   chat: traceable(
     async (params) => {
       try {
         const { prompt, sessionId } = params;
-
-        //   strapi.log.debug(JSON.stringify(params));
 
         const vectorStore = await PineconeStore.fromExistingIndex(
           textEmbedding3Small,
@@ -65,7 +51,6 @@ const Service = () => ({
             pineconeIndex,
           }
         );
-        // const documentContents = "Brief answer";
 
         const retriever = vectorStore.asRetriever();
 
@@ -77,46 +62,12 @@ const Service = () => ({
 
         const tools = [searchTool, retrieverTool];
 
-        // const selfQueryRetriever = SelfQueryRetriever.fromLLM({
-        //   llm,
-        //   vectorStore,
-        //   documentContents,
-        //   attributeInfo,
-        //   structuredQueryTranslator: new PineconeTranslator(),
-        // });
-
-        //   const query = await selfQueryRetriever.invoke(prompt);
-
-        // const systemTemplate = "Translate the following into {language}:";
-
         const promptTemplate = ChatPromptTemplate.fromMessages([
           ["system", "You are a helpful assistant"],
           ["placeholder", "{chat_history}"],
           ["human", "{input}"],
           ["placeholder", "{agent_scratchpad}"],
         ]);
-
-        // const parser = new StringOutputParser();
-
-        // const chain = promptTemplate.pipe(llm);
-
-        // const withMessageHistory = new RunnableWithMessageHistory({
-        //   runnable: chain,
-        //   getMessageHistory: async (sessionId) => {
-        //     if (messageHistories[sessionId] === undefined) {
-        //       messageHistories[sessionId] = new InMemoryChatMessageHistory();
-        //     }
-        //     return messageHistories[sessionId];
-        //   },
-        //   inputMessagesKey: "input",
-        //   historyMessagesKey: "chat_history",
-        // });
-
-        // const config = {
-        //   configurable: {
-        //     sessionId: sessionId,
-        //   },
-        // };
 
         const agent = await createOpenAIFunctionsAgent({
           llm,
@@ -151,14 +102,6 @@ const Service = () => ({
             },
           }
         );
-        // const response = await withMessageHistory.invoke(
-        //   {
-        //     input: prompt,
-        //   },
-        //   config
-        // );
-
-        // const result = await llmWithTracing.invoke("Hello, world!");
 
         return result;
       } catch (error) {
